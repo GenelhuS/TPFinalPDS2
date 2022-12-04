e72e9d2af1b3669a7feca3bcf7895289f24b7bff
GenelhuS <saulo.matos2013@gmail.com>
2022-12-04 09:42:41 -0300

    Tirando o metodo deletar e adicionar produto da classe ecommerce, pois essa funcionalidade ja ta nos metodos da classe vendedor

--- include/ecommerce.hpp
+++ include/ecommerce.hpp
@@ -17,8 +17,6 @@ public:
     void cadastro_vendedor(std::string email, std::string senha, std::string nome, int cpf, int telefone, std::string data_nasc, std::string sexo);
     void imprimir_produtos();
     void imprimir_dados(int cpf);
-    void Cadastrar_produto();
-    void deletar_produto();
     int login_cliente(std::string email, std::string senha);
     int login_vendedor(std::string email, std::string senha);
 
