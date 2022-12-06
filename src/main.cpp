b2f4717122ed916050997cbed094ad90638887f2
GenelhuS <saulo.matos2013@gmail.com>
2022-12-06 01:50:48 -0300

    Continuando o processo de montagem da main

--- src/main.cpp
+++ src/main.cpp
@@ -11,7 +11,7 @@ void main(){
     int opcao;
     Ecommerce ecommerce = Ecommerce();
     std::cout<<"Bem vindo ao sistema de loja virtual Ecommerce"<<std::endl;
-    std::cout<<"Digite 1 para criar sua conta ou 2 para se cadastrar";
+    std::cout<<"Digite 1 para criar sua conta ou 2 para fazer login";
     while(std::cin>>opcao){
         switch(opcao){
             //cadastro na plataforma
@@ -20,6 +20,7 @@ void main(){
                 int controleMenuCadastro;
 
                 std::cout<<"Digite 1 para cadastrar cliente e 2 para cadastrar vendedor";
+                std::cin>>controleMenuCadastro;
 
                 switch(controleMenuCadastro){
                     case 1:{
@@ -66,8 +67,53 @@ void main(){
 
             //login na plataforma, como cliente ou vendedor
             case 2:{
+                int controleMenuLogin;
+                std::cout<<"Digite 1 para fazer login como cliente ou 2 para fazer login como vendedor:"<<std::endl;
+                std::cin>>controleMenuLogin;
 
-            }
+                switch (controleMenuLogin){
+
+                    case 1:{
+                        std::string email; std::string senha;
+                        int idCliente;
+                        do{
+                            std::cout<<"Digite seu EMAIL e SENHA de cliente separados por um espaco:"<<std::endl;
+                            std::cin>>email>>senha;
+
+                            idCliente = ecommerce.login_cliente(email,senha);
+                            if(idCliente==0){
+                                std::cout<<"Cliente não existe, tente novamente";
+                            }
+                            else{
+                                int controleMenuCliente;
+                                std::cout<<"Digite 1 para ver os produtos cadastrados e fazer a sua compra ou digite 2 para ver os dados do seu perfil:";
+                                std::cin>>controleMenuCliente;
+                                switch(controleMenuCliente){
+                                    case 1:{
+                                        ecommerce.imprimir_produtos();
+                                    }break;
+                                    case 2:{
+                                        int cpf;
+                                        std::cout<<"Confirme seu CPF:"<<std::endl;
+                                        std::cin>>cpf;
+                                        ecommerce.imprimir_dados(cpf);
+                                    }
+                                }
+                            }
+                        }while(idCliente==0);
+
+
+                    }break;
+
+                    case 2:{
+                        //teste
+                        int i;
+                    }break;
+                
+
+                }
+
+            }break;
         }
     }
 }
