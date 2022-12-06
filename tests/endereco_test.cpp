bbd5da3d00fb51e3d406ca49f0b44f80b706831e
Miguel Sousa <msousamorais01@gmail.com>
2022-12-05 22:27:25 -0300

    implementação do produto test

--- tests/endereco_test.cpp
+++ tests/endereco_test.cpp
@@ -13,5 +13,4 @@ Endereco teste(31485611,"Brasil",1009,"Avenida dos testes");
     CHECK_EQ(teste.getNum() , 1009);
     CHECK_EQ(teste.getRua() , "Avenida dos testes");     // se todas as asserções passam, construtor
                                                          // e gets funcionam corretamente
-
 }
